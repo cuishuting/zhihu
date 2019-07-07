5e57259a92c8d84b971655a6e2e85a577220e112
cuishuting <1404167547@qq.com>
2019-07-07 15:48:33 +0800

    refresh2

--- src/pages/newest.vue
+++ src/pages/newest.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <ul>
-            <li v-for="(item,key) in data">
-                <artical :data></artical>
-            </li>
-        </ul>
+        <div v-if="!success">
+            <img src="../img/没有内容的哭脸表情.png" style="background-color: white">
+            <p style="font-size: 30px">已没有更多内容</p>
+        </div>
+        <div v-else>
+            <ul>
+                <li v-for="(item,key) in data " :key="key">
+                    <artical :data="item" ></artical>
+                </li>
+            </ul>
+        </div>
+
 
     </div>
 </template>
-
 <script>
     import artical from '../components/artical.vue';
     export default {
@@ -33,6 +39,9 @@
                     this.success=response.success;
                     if(!this.success){
                         this.error=response.error;
+                    }
+                    else {
+
                     }
                     this.hasMore=response.hasMore;
                     this.data=response.data;

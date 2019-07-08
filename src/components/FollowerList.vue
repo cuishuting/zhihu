c9f2aa562379697dab27c3370cee82a69a9f04df
cuishuting <1404167547@qq.com>
2019-07-08 10:12:00 +0800

    refresh about the components:Followers&FollowerList

--- src/components/FollowerList.vue
+++ src/components/FollowerList.vue
@@ -13,9 +13,14 @@
         components:{
             Followers,
         },
+        props:[
+            'user_id',
+        ],
         data(){
           return {
               data:[],
+              success:false,
+              error:'',
           }
         },
         methods:{
@@ -28,9 +33,28 @@
                     followers_count:30,
                     isFollow:false,
                 };
+                let data2={
+                    username:'cuishuting',
+                    head_sculpture:'https://i.loli.net/2017/08/21/599a521472424.jpg',
+                    intro:'这里是个人简介的部分啊啊啊啊',
+                    answer_count:10,
+                    followers_count:30,
+                    isFollow:false,
+                };
                 this.data.push(data1);
-
+                this.data.push(data2);
             },
+            get_followers_info(){
+                this.axios.get('/api/get_followees'+ '?user_id='+this.user_id).then((response)=>{
+                    this.success=response.data.success;
+                    if(!this.success){
+                        this.$Message.error(response.data.error);
+                    }
+                    else {
+                        this.data=response.data.data;
+                    }
+                })
+            }
         },
         mounted:function(){
             this.fakeData();
--- src/components/Followers.vue
+++ src/components/Followers.vue
@@ -1,11 +1,13 @@
 <template>
-    <Card style="width:320px">
+    <Card  class="follower">
         <div style="text-align:left">
-            <img :src="head_sculpture" class="head">
-            <p>{{username}}</p>
-            <p>{{intro}}</p>
-            <p>{{answer_count}}回答•{{followers_count}}关注者</p>
-            <Button :size="large" icon="md-add" type="primary" @click="Follow" ><span v-if="!isFollow">关注他</span><span v-if="isFollow">取消关注</span></Button>
+            <img :src="head_sculpture" class="head" @click="jumpToUser">
+            <div class="info">
+                <p style="font-weight: bold;font-size:20px">{{username}}</p>
+                <p style="font-size: 15px">{{intro}}</p>
+                <p style="color:gray;font-size:14px">{{answer_count}}回答•{{followers_count}}关注者</p>
+            </div>
+            <Button :size="large" icon="md-add" type="primary" @click="Follow" class="button"><span v-if="!isFollow">关注他</span><span v-if="isFollow">取消关注</span></Button>
         </div>
     </Card>
 </template>
@@ -26,7 +28,6 @@
             }
         },
         methods:{
-
             Follow(){
                 if(!this.isFollow) {
                     this.axios.post('/api/follow',{"user_id":this.userid}).then((resp)=>{
@@ -57,6 +58,9 @@
                     })
                 }
 
+            },
+            jumpToUser(){
+                this.$router.push('/user/:id');
             }
         },
         mounted:function() {
@@ -68,5 +72,19 @@
 <style scoped>
     .head{
         float:left;
+        width:90px;
+        height:90px;
+    }
+    .follower{
+        width:60%;
+        margin-left:10%;
+    }
+    .button{
+        margin-top:-20px;
+        margin-left:85%;
+    }
+    .info{
+        line-height: 35px;
+        margin-left:15%;
     }
 </style>
